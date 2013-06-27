fdf3b8b6fac5971fd0dc96f121d8780390b10d61
r46089 <fxchip@zcarlson-macbook-II.local>
2013-06-26 21:01:55 -0700

    smallestWhileGreater function
    Checks a passed array for the smallest value still greater than a
    passed-in value

--- copiouslycopasetic.js
+++ copiouslycopasetic.js
@@ -1,3 +1,22 @@
 
 // Copiously Copasetic
 
+function smallestWhileGreater(inArray, greaterThanValue) {
+	if (inArray.length) {
+		var i = 0;
+		var lowest = NaN;
+		for (i = 0; i < inArray.length; i++) {
+			if (inArray[i] <= greaterThanValue) continue;
+
+			// equal or greater -> see if lowest is greater
+			// if lowest is greater, set lowest to value
+			if (lowest > inArray[i] || isNaN(lowest)) {
+				lowest = inArray[i];
+			}
+		}
+		return lowest;
+	} else {
+		return -1;
+	}
+}
+
